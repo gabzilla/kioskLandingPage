31db30be5084fdf08d544ccc2c8414846a8696e7
Gabby Jameson <gjameson305@gmail.com>
2016-05-19 00:32:50 -0700

    goto() used to launch webpage after user input; toggleFullScreen() used to force full screen mode (not i.e. or safari supported)

---
+++ index.js
@@ -0,0 +1,37 @@
+function goTo() {
+  var url = document.forms[0].url.value;
+  myWindow = window.open("http://"+url, "_blank");
+  myWindow.focus(); // sends window to front
+  return false;
+}
+function toggleFullScreen(elem) {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) || (document.msfullscreenElement && document.msfullscreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+      if (elem.requestFullScreen) {
+        elem.requestFullScreen();
+      } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullScreen) {
+        elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+      } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+      }
+    } else {
+      if (document.cancelFullScreen) {
+        document.cancelFullScreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+  }
+}
+
+
+
+/* https://developer.mozilla.org/en-US/docs/Web/API/Document/mozFullScreen
+m   methods used for creating full screen effect is experimental and does not work on safari or ie
+ */
